1be0cd4461979a3d9219cc2ac898d7aee4e50996
Jared <jared_cambier@brown.edu>
2022-11-18 00:00:13 -0500

    good progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import './App.css';
+import './Pokemon.js';
+import Pokemon from './Pokemon.js';
 
-tangrowthData = [""];
-turtwigData = [""];
-meganiumData = [""];
-leafeonData = [""];
+const tangrowthData = ["Grass", "Impish", "100", "535", "Tangrowth"];
+const turtwigData = ["Grass", "Adamant", "68", "318", "Turtwig"];
+const meganiumData = ["Grass", "Bold", "82", "525", "Meganium"];
+const leafeonData = ["Grass", "Impish", "110", "525", "Leafeon"];
 
-politoedData = [""];
-miloticData = [""];
-totodileData = [""];
-vaporeonData = [""];
+const politoedData = ["Water", "Bold", "75", "500", "Politoed"];
+const miloticData = ["Water", "Bold", "60", "540", "Milotic"];
+const totodileData = ["Water", "Adamant", "65", "314", "Totodile"];
+const vaporeonData = ["Water", "Bold", "65", "525", "Vaporeon"];
 
-heatmorData = [""];
-torkoalData = [""];
-chimcharData = [""];
-flareonData = [""];
+const heatmorData = ["Fire", "Impish", "97", "484", "Heatmor"];
+const torkoalData = ["Fire", "Impish", "85", "470", "Torkoal"];
+const chimcharData = ["Fire", "Adamant", "58", "309", "Chimchar"];
+const flareonData = ["Fire", "Adamant", "130", "525", "Flareon"];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Create Your Own Team of Pokemon!</header>
+      <div className="functions">
+        <div className="options">
+          <h1>Sort</h1>
+          <select>
+            <option>None</option>
+            <option>Highest Attack</option>
+          </select>
+        </div>
+        <div className="options">
+          <h1>Filter</h1>
+          <div className="filter-category">
+            <p className="filter-text">Type:</p>
+            <select>
+              <option>None</option>
+              <option>Grass</option>
+              <option>Water</option>
+              <option>Fire</option>
+            </select>
+          </div>
+          <div className="filter-category">
+            <p className="filter-text">Nature:</p>
+            <select>
+              <option>None</option>
+              <option>Adamant</option>
+              <option>Bold</option>
+              <option>Impish</option>
+            </select>
+          </div>
+        </div>
+        <div className="options">
+          <h1>Party</h1>
+          <button>See Party</button>
+          <button>Clear Party</button>
+        </div>
+      </div>
+      <div className="pokemon-cards">
+        <Pokemon data={tangrowthData}></Pokemon>
+        <Pokemon data={turtwigData}></Pokemon>
+        <Pokemon data={meganiumData}></Pokemon>
+        <Pokemon data={leafeonData}></Pokemon>
+        <Pokemon data={politoedData}></Pokemon>
+        <Pokemon data={miloticData}></Pokemon>
+        <Pokemon data={totodileData}></Pokemon>
+        <Pokemon data={vaporeonData}></Pokemon>
+        <Pokemon data={heatmorData}></Pokemon>
+        <Pokemon data={torkoalData}></Pokemon>
+        <Pokemon data={chimcharData}></Pokemon>
+        <Pokemon data={flareonData}></Pokemon>
+      </div>
     </div>
   );
 }
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,13 +1,75 @@
 import Card from '@mui/material/Card';
+import { Typography } from '@mui/material';
+import { Button } from '@mui/material';
+import './Pokemon.css';
+import tangrowth from './images/tangrowth.webp';
+import chimchar from './images/chimchar.png';
+import flareon from './images/flareon.webp';
+import heatmor from './images/heatmor.webp';
+import leafeon from './images/leafeon.jpg';
+import meganium from './images/meganium.png';
+import milotic from './images/milotic.webp';
+import politoed from './images/politoed.jpg';
+import torkoal from './images/torkoal.webp';
+import totodile from './images/totodile.png';
+import turtwig from './images/turtwig.png';
+import vaporeon from './images/vaporeon.png';
+
+function checkImage(name) {
+    switch(name) {
+        case "Tangrowth":
+            return tangrowth;
+        case "Chimchar":
+            return chimchar;
+        case "Flareon":
+            return flareon;
+        case "Heatmor":
+            return heatmor;
+        case "Leafeon":
+            return leafeon;
+        case "Meganium":
+            return meganium;
+        case "Milotic":
+            return milotic;
+        case "Politoed":
+            return politoed;
+        case "Torkoal":
+            return torkoal;
+        case "Totodile":
+            return totodile;
+        case "Turtwig":
+            return turtwig;
+        default:
+            return vaporeon;
+        }
+}
 
 export default function Pokemon(props) {
-    data = props.data;
-    image = data[0];
-    type = data[1];
-    nature = data[2];
-    attack = data[3];
-    total = data[4];
+    const data = props.data;
+    const type = data[0];
+    const nature = data[1];
+    const attack = data[2];
+    const total = data[3];
+    const pName = data[4];
+    const image = checkImage(pName);
     return (
-        <Card></Card>
+        <Card className="card">
+            <div className="pokemon-info">
+                <Typography variant="h5" sx={{fontWeight: 'bold'}}>{pName}</Typography>
+                <Typography variant="subtitle1">Type: {type}</Typography>
+                <Typography variant="subtitle1">Nature: {nature}</Typography>
+                <Typography variant="subtitle1">Attack: {attack}</Typography>
+                <Typography variant="subtitle1">Total: {total}</Typography>
+                <div className="button">
+                    <Button variant="contained" size="small">Add To Party</Button>
+                </div>
+                <div className="button">
+                    <Button variant="contained" size="small" className="button">Remove From Party</Button>
+                </div>
+            </div>
+            <div className="pokemon-image">
+                <img src={image}></img>
+            </div>
+        </Card>
     );
 }
